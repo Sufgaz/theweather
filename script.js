abeb4c627977d2fe6843036d015858869891b9a6
sufgaz <sufgaz911@gmail.com>
2024-01-14 17:21:56 +0000

    API key and event listner added

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    //OpenWeatherMap API key
+    const apiKey = '6ba3c11c0b5f3f3af090135e1c336ac8';
+  
+    //Event listener for the search form
+    $('#search-form').submit(function (event) {
+      event.preventDefault();
+    });
+});
